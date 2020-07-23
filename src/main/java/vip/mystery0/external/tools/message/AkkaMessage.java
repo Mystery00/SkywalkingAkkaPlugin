2ca8d47e9dce0ce60bfa98bd54eb98009d7280cf
Mystery0 <mystery0dyl520@gmail.com>
2020-07-23 15:23:29 +0800

    2

--- src/main/java/vip/mystery0/external/tools/message/AkkaMessage.java
+++ src/main/java/vip/mystery0/external/tools/message/AkkaMessage.java
@@ -1,8 +1,5 @@
 package vip.mystery0.external.tools.message;
 
-import org.slf4j.MDC;
-import vip.mystery0.external.tools.config.Constants;
-
 import java.io.Serializable;
 
 /**
@@ -12,19 +9,9 @@ import java.io.Serializable;
 public class AkkaMessage<T> implements Serializable {
     private T data;
     private String traceId;
-    private Object metadata;
 
     public AkkaMessage(T data) {
         this.data = data;
-        captureTrace();
-    }
-
-    public void captureTrace() {
-        this.traceId = MDC.get(Constants.MDC_TRACE_ID);
-    }
-
-    public void restoreTrace() {
-        MDC.put(Constants.MDC_TRACE_ID, this.traceId);
     }
 
     public T getData() {
@@ -41,14 +28,6 @@ public class AkkaMessage<T> implements Serializable {
 
     public void setTraceId(String traceId) {
         this.traceId = traceId;
-    }
-
-    public Object getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Object metadata) {
-        this.metadata = metadata;
     }
 
     @Override
